dba3f22bac46f8c834f5d7a93a76cb608dec869b
harshita22076@gmail.com <harshita22076@gmail.com>
2025-10-08 17:17:28 +0530

    after base url

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const emailRoutes = require("./routes/emailRoutes");
 const errorHandler = require("./middleware/errorHandler");
 const connectDB = require("./config/db");
-connectDB(); 
+connectDB();
 const app = express();
 
 
@@ -13,8 +13,15 @@ app.use(bodyParser.json());
 
 // Routes
 app.use("/api/email", emailRoutes);
+app.get('/', (req, res) => {
+    res.status(200).json({
+        status: 1,
+        message: 'Welcome to the Email Verification API!'
+    });
+});
 
 // Global Error Handler
-app.use(errorHandler);
+// app.use(errorHandler);
+
 
 module.exports = app;
--- src/routes/emailRoutes.js
+++ src/routes/emailRoutes.js
@@ -2,9 +2,6 @@ const express = require("express");
 const router = express.Router();
 const { sendOTP, verifyOTP } = require("../controllers/emailController");
 
-router.get("/", (req, res)=>{
-    res.send("Hello from server");
-})
 router.post("/send-otp", sendOTP);
 router.post("/verify-otp", verifyOTP);
 
