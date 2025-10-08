14506351df70698810a1373ef482555cc7412b13
harshita22076@gmail.com <harshita22076@gmail.com>
2025-10-08 16:59:21 +0530

    first commit

--- src/routes/emailRoutes.js
+++ src/routes/emailRoutes.js
@@ -2,6 +2,9 @@ const express = require("express");
 const router = express.Router();
 const { sendOTP, verifyOTP } = require("../controllers/emailController");
 
+router.get("/", (req, res)=>{
+    res.send("Hello from server");
+})
 router.post("/send-otp", sendOTP);
 router.post("/verify-otp", verifyOTP);
 
